b2177992d40d6672d8f15d675330f5268c1bf522
Flávio Caetano <flavio@vieiracaetano.com>
2013-01-16 17:57:43 -0200

    render_to_string() bugfix.

--- django_markdown/views.py
+++ django_markdown/views.py
@@ -1,10 +1,10 @@
-from django.views.generic.simple import direct_to_template
+from django.shortcuts import render
 from django.conf import settings
 
 def preview(request):
     media_or_static = settings.STATIC_URL or settings.MEDIA_URL
     css = getattr(settings, 'DJANGO_MARKDOWN_STYLE', media_or_static+'django_markdown/preview.css')
 
-    return direct_to_template(
+    return render(
         request, 'django_markdown/preview.html',
         {'content': request.REQUEST.get('data', 'No content posted'), 'css': css})
